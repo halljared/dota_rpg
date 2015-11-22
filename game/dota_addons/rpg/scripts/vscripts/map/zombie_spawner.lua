b6369b8a02f582c448d1a3aa9d431e1163fe7f49
Jared Hall <halljared1@gmail.com>
2015-11-22 16:49:09 -0500

    Minor map changes; begin witch doctor rebuild

--- game/dota_addons/rpg/scripts/vscripts/map/zombie_spawner.lua
+++ game/dota_addons/rpg/scripts/vscripts/map/zombie_spawner.lua
@@ -11,7 +11,7 @@ function Spawn(args)
 	local spawnLocation = Entities:FindByName( nil, "zombie_spawner" ):GetAbsOrigin()
 	Timers:CreateTimer(0, function()
 			--local units = {'npc_dota_creature_ancient_apparition', 'npc_dota_creature_corpselord'}
-			local units = {'npc_dota_hero_lycan'}
+			local units = {'npc_dota_creature_berserk_zombie'}
 			local unit = units[ RandomInt(1, #units) ]
 			unit = CreateUnitByName( unit, spawnLocation, true, nil, nil, DOTA_TEAM_BADGUYS )
 			return 20
