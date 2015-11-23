5e283b41ee890ce897ccec3c1cf0abe76e2e1e33
Jared Hall <halljared1@gmail.com>
2015-11-23 16:15:38 -0500

    Fixed test npc spawner

--- game/dota_addons/rpg/scripts/vscripts/map/zombie_spawner.lua
+++ game/dota_addons/rpg/scripts/vscripts/map/zombie_spawner.lua
@@ -8,6 +8,7 @@ function Enable(args)
 end
 
 function Spawn(args)
+	print('spawn')
 	local spawnLocation = Entities:FindByName( nil, "zombie_spawner" ):GetAbsOrigin()
 	Timers:CreateTimer(0, function()
 			--local units = {'npc_dota_creature_ancient_apparition', 'npc_dota_creature_corpselord'}
@@ -28,4 +29,11 @@ function Activate(args)
 	end
 end
 
-print('global')
+function SpawnNPC(args)
+	print('spawnnpc')
+	if args then
+		for k,v in pairs(args) do
+			print (args)
+		end
+	end
+end
